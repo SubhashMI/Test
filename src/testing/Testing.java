9fc536b625f5b05eed28d363ec9e3f10eef9a733
SubhashMI <mishsubh@sheridancollege.ca>
2023-02-10 19:39:46 -0500

    Second step

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -14,7 +14,9 @@ public class Testing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //now adding in netbeans
+        System.out.println("Netbeans");
     }
     
 }
