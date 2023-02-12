2fb0a33acee4ad314da358ffea27db3165f6c88b
SubhashMI <mishsubh@sheridancollege.ca>
2023-02-12 13:10:58 -0500

    first change

---
+++ src/testing1/Testing1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testing1;
+
+/**
+ *
+ * @author subha
+ */
+public class Testing1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int x = 2;
+       int y = 3;
+       int z = x*y;
+       System.out.println("Output: "+z);
+    }
+    
+}
